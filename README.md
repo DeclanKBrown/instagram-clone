4b902c60162256ede8d37ef35207aeb9e4962c2c
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-11-18 00:57:29 +1100

    MERN template added

---
+++ README.md
@@ -0,0 +1,6 @@
+MERN Stack Instagram Clone
+
+Front End | Vite React + TypeScript
+
+Back End | Node + Express TypeScript - MVC Paradigm
+
---
+++ api/src/app.ts
@@ -0,0 +1,56 @@
+import createError, { HttpError } from 'http-errors'
+import express, { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import router from './routes/index'
+
+dotenv.config()
+
+// Set up MongoDB
+mongoose.set('strictQuery', false)
+const mongoDB: string | undefined = process.env.MONGODB_URI
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(mongoDB!)
+}
+
+const app = express()
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', router)
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  res.status(err.status || 500)
+})
+
+export default app
